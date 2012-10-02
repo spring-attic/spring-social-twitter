689524bae3cc913ce51e3e55a8407f191396d890
Jeremy D. Appel <jdlappel@gmail.com>
2012-10-01 22:15:24 -0400

    Updated Geo related functionality to API v1.1

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/GeoTemplate.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/GeoTemplate.java
@@ -36,6 +36,7 @@ public class GeoTemplate extends AbstractTwitterOperations implements GeoOperati
 	}
 
 	public Place getPlace(String placeId) {
+		requireAuthorization();
 		return restTemplate.getForObject(buildUri("geo/id/" + placeId + ".json"), Place.class);
 	}
 	
@@ -44,6 +45,7 @@ public class GeoTemplate extends AbstractTwitterOperations implements GeoOperati
 	}
 	
 	public List<Place> reverseGeoCode(double latitude, double longitude, PlaceType granularity, String accuracy) {
+		requireAuthorization();
 		MultiValueMap<String, String> parameters = buildGeoParameters(latitude, longitude, granularity, accuracy, null);
 		return restTemplate.getForObject(buildUri("geo/reverse_geocode.json", parameters), PlacesList.class).getList();
 	}
@@ -53,6 +55,7 @@ public class GeoTemplate extends AbstractTwitterOperations implements GeoOperati
 	}
 	
 	public List<Place> search(double latitude, double longitude, PlaceType granularity, String accuracy, String query) {
+		requireAuthorization();
 		MultiValueMap<String, String> parameters = buildGeoParameters(latitude, longitude, granularity, accuracy, query);
 		return restTemplate.getForObject(buildUri("geo/search.json", parameters), PlacesList.class).getList();
 	}
@@ -62,6 +65,7 @@ public class GeoTemplate extends AbstractTwitterOperations implements GeoOperati
 	}
 	
 	public SimilarPlaces findSimilarPlaces(double latitude, double longitude, String name, String streetAddress, String containedWithin) {
+		requireAuthorization();
 		MultiValueMap<String, String> parameters = buildPlaceParameters(latitude, longitude, name, streetAddress, containedWithin);
 		SimilarPlacesResponse response = restTemplate.getForObject(buildUri("geo/similar_places.json", parameters), SimilarPlacesResponse.class);
 		PlacePrototype placePrototype = new PlacePrototype(response.getToken(), latitude, longitude, name, streetAddress, containedWithin);
@@ -70,10 +74,11 @@ public class GeoTemplate extends AbstractTwitterOperations implements GeoOperati
 	}
 	
 	public Place createPlace(PlacePrototype placePrototype) {
+		requireAuthorization();
 		MultiValueMap<String, String> request = buildPlaceParameters(
 				placePrototype.getLatitude(), placePrototype.getLongitude(), placePrototype.getName(), placePrototype.getStreetAddress(), placePrototype.getContainedWithin());
 		request.set("token", placePrototype.getCreateToken());
-		return restTemplate.postForObject("https://api.twitter.com/1/geo/place.json", request, Place.class);		
+		return restTemplate.postForObject("https://api.twitter.com/1.1/geo/place.json", request, Place.class);		
 	}
 	
 	// private helpers
