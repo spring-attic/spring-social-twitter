bf63cffd8140b8c3baebca58550f2df5bb57ad23
Jeremy D. Appel <jdlappel@gmail.com>
2012-11-19 20:26:34 -0500

    Fix rate_limit endpoint and polish

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/DirectMessageTemplate.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/DirectMessageTemplate.java
@@ -16,7 +16,6 @@
 package org.springframework.social.twitter.api.impl;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.social.twitter.api.DirectMessage;
 import org.springframework.social.twitter.api.DirectMessageOperations;
