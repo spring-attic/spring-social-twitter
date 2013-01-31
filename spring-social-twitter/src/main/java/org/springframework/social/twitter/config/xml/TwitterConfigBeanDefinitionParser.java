609e977eceeeb9dd818114de557b85d408a61380
Craig Walls <cwalls@vmware.com>
2013-01-31 16:40:37 -0600

    Consolidated UserIdExtractor and UserIdSource in core, requiring adjustments in Spring Social Twitter

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/config/xml/TwitterConfigBeanDefinitionParser.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/config/xml/TwitterConfigBeanDefinitionParser.java
@@ -17,9 +17,9 @@ package org.springframework.social.twitter.config.xml;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.xml.AbstractProviderConfigBeanDefinitionParser;
 import org.springframework.social.config.xml.ApiHelper;
-import org.springframework.social.config.xml.UserIdSource;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.twitter.api.Twitter;
